fc10d97fe6e709d7da191a7768867a0ddd18e84a
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-22 16:04:36 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,48 +1,84 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { Header, Form, Button, Span, Input } from './Searchbar.styled';
 import { toast } from 'react-toastify';
 
-export class Searchbar extends Component {
-  state = { photoTag: '' };
+export function Searchbar({ onSubmit }) {
+  const [photoTag, setPhotoTag] = useState('');
 
-  handleChange = e => {
-    this.setState({ photoTag: e.currentTarget.value });
+  const handleChange = e => {
+    setPhotoTag(e.currentTarget.value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.photoTag.trim() === '') {
+    if (photoTag.trim() === '') {
       toast.info('Enter your search query');
       return;
     }
-    this.props.onSubmit(this.state.photoTag);
-
-    this.setState({ photoTag: '' });
+    onSubmit(photoTag);
+    setPhotoTag('');
   };
 
-  render() {
-    const { handleSubmit, handleChange } = this;
-    return (
-      <Header>
-        <Form onSubmit={handleSubmit}>
-          <Button type="submit">
-            <Span>Search</Span>
-          </Button>
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">
+          <Span>Search</Span>
+        </Button>
 
-          <Input
-            type="text"
-            autoComplete="off"
-            value={this.state.photoTag}
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={handleChange}
-          />
-        </Form>
-      </Header>
-    );
-  }
+        <Input
+          type="text"
+          autoComplete="off"
+          value={photoTag}
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleChange}
+        />
+      </Form>
+    </Header>
+  );
 }
+// export class Searchbar extends Component {
+//   state = { photoTag: '' };
+
+//   handleChange = e => {
+//     this.setState({ photoTag: e.currentTarget.value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     if (this.state.photoTag.trim() === '') {
+//       toast.info('Enter your search query');
+//       return;
+//     }
+//     this.props.onSubmit(this.state.photoTag);
+
+//     this.setState({ photoTag: '' });
+//   };
+
+//   render() {
+//     const { handleSubmit, handleChange } = this;
+//     return (
+//       <Header>
+//         <Form onSubmit={handleSubmit}>
+//           <Button type="submit">
+//             <Span>Search</Span>
+//           </Button>
+
+//           <Input
+//             type="text"
+//             autoComplete="off"
+//             value={this.state.photoTag}
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={handleChange}
+//           />
+//         </Form>
+//       </Header>
+//     );
+//   }
+// }
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
