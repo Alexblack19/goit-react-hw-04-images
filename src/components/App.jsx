97125702d2119e212ebbe3d137d3fcbaa47e4702
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-23 19:10:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Notiflix from 'notiflix';
-import { Component } from 'react';
+import { setState } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Searchbar } from './Searchbar/Searchbar';
 import { getAllPhoto, NUM_REQUESTED_PHOTOS } from '../api/image-api';
@@ -10,19 +10,22 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
 import { Modal } from './Modal/Modal';
 
-export class App extends Component {
-  state = {
-    dataPhoto: null,
-    error: '',
-    page: 1,
-    showModal: false,
-    photoTag: '',
-    isLoading: false,
-    currentLargeImageUrl: '',
-    currentImageTags: '',
-    currentHits: null,
-    totalHits: null,
-  };
+export function App() {
+  const[dataPhoto, setDataPhoto]=setState(null);
+  const[error, setError]=setState('');
+
+  // state = {
+  //   dataPhoto: null,
+  //   error: '',
+  //   page: 1,
+  //   showModal: false,
+  //   photoTag: '',
+  //   isLoading: false,
+  //   currentLargeImageUrl: '',
+  //   currentImageTags: '',
+  //   currentHits: null,
+  //   totalHits: null,
+  // };
 
   componentDidUpdate(_, prevState) {
     const searchTag = this.state.photoTag;
@@ -32,7 +35,7 @@ export class App extends Component {
     }
   }
 
-  fetchPhoto = async (searchTag, page) => {
+  const fetchPhoto = async (searchTag, page) => {
     this.setState({ isLoading: true });
     try {
       const data = await getAllPhoto(searchTag, page);
@@ -47,40 +50,40 @@ export class App extends Component {
       this.setState({ totalHits: data.totalHits });
 
       if (data.hits.length === 0) {
-        this.notificationTry();
+        notificationTry();
       }
     } catch (error) {
       this.setState({ error: error.message });
-      this.notificationCatch(error.message);
+      notificationCatch(error.message);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
-  openModal = e => {
+  const openModal = e => {
     const currentLargeImageUrl = e.target.dataset.large;
     const currentImageTags = e.target.alt;
 
     this.setState({ currentLargeImageUrl, currentImageTags });
-    this.toggleModal();
+    toggleModal();
   };
 
-  toggleModal = () => {
+  const toggleModal = () => {
     this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
 
-  handleFormSubmit = photoTag => {
+  const handleFormSubmit = photoTag => {
     this.setState({ photoTag });
     this.setState({ page: 1 });
   };
 
-  handleLoadMore = () => {
+  const handleLoadMore = () => {
     const page = this.state.page + 1;
     this.setState({ page: page });
-    this.fetchPhoto(this.state.photoTag, page);
+    fetchPhoto(this.state.photoTag, page);
   };
 
-  notificationTry() {
+  function notificationTry() {
     Notiflix.Notify.warning(
       'Sorry, there are no images matching your search query. Please try again.',
       {
@@ -93,27 +96,105 @@ export class App extends Component {
     );
   }
 
-  notificationCatch(error) {
+  function notificationCatch(error) {
     Notiflix.Notify.warning(error, {
       position: 'center-center',
       fontSize: '16px',
       width: '340px',
     });
   }
+// export class App extends Component {
+//   state = {
+//     dataPhoto: null,
+//     error: '',
+//     page: 1,
+//     showModal: false,
+//     photoTag: '',
+//     isLoading: false,
+//     currentLargeImageUrl: '',
+//     currentImageTags: '',
+//     currentHits: null,
+//     totalHits: null,
+//   };
 
-  render() {
-    const {
-      showModal,
-      dataPhoto,
-      isLoading,
-      currentLargeImageUrl,
-      currentImageTags,
-      currentHits,
-      totalHits,
-    } = this.state;
-    const { handleFormSubmit, toggleModal, handleLoadMore, openModal } = this;
+//   componentDidUpdate(_, prevState) {
+//     const searchTag = this.state.photoTag;
+//     if (prevState.photoTag !== searchTag) {
+//       this.setState({ dataPhoto: null });
+//       this.fetchPhoto(searchTag, this.state.page);
+//     }
+//   }
 
-    return (
+//   fetchPhoto = async (searchTag, page) => {
+//     this.setState({ isLoading: true });
+//     try {
+//       const data = await getAllPhoto(searchTag, page);
+
+//       if (!this.state.dataPhoto) {
+//         this.setState({ dataPhoto: data.hits });
+//       } else {
+//         this.setState({ dataPhoto: [...this.state.dataPhoto, ...data.hits] });
+//       }
+
+//       this.setState({ currentHits: NUM_REQUESTED_PHOTOS * this.state.page });
+//       this.setState({ totalHits: data.totalHits });
+
+//       if (data.hits.length === 0) {
+//         this.notificationTry();
+//       }
+//     } catch (error) {
+//       this.setState({ error: error.message });
+//       this.notificationCatch(error.message);
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   openModal = e => {
+//     const currentLargeImageUrl = e.target.dataset.large;
+//     const currentImageTags = e.target.alt;
+
+//     this.setState({ currentLargeImageUrl, currentImageTags });
+//     this.toggleModal();
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({ showModal: !showModal }));
+//   };
+
+//   handleFormSubmit = photoTag => {
+//     this.setState({ photoTag });
+//     this.setState({ page: 1 });
+//   };
+
+//   handleLoadMore = () => {
+//     const page = this.state.page + 1;
+//     this.setState({ page: page });
+//     this.fetchPhoto(this.state.photoTag, page);
+//   };
+
+//   notificationTry() {
+//     Notiflix.Notify.warning(
+//       'Sorry, there are no images matching your search query. Please try again.',
+//       {
+//         position: 'center-center',
+//         fontSize: '18px',
+//         cssAnimationStyle: 'zoom',
+//         cssAnimationDuration: 1000,
+//         width: '380px',
+//       }
+//     );
+//   }
+
+//   notificationCatch(error) {
+//     Notiflix.Notify.warning(error, {
+//       position: 'center-center',
+//       fontSize: '16px',
+//       width: '340px',
+//     });
+//   }
+
+      return (
       <div>
         <GlobalStyle />
         <Searchbar onSubmit={handleFormSubmit} />
@@ -130,7 +211,7 @@ export class App extends Component {
           <Button handleLoadMore={handleLoadMore} />
         )}
         <ToastContainer autoClose={3000} />
-      </div>
-    );
+      </div>)
+    
   }
-}
+
