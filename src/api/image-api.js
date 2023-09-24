857f837fbefdf478f483f9e65950521414833cac
Alexblack19 <107646776+Alexblack19@users.noreply.github.com>
2023-09-24 21:41:26 +0300

    Update image-api.js

--- src/api/image-api.js
+++ src/api/image-api.js
@@ -4,8 +4,7 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38572739-8ecec7d616fae8b4ce60f4b21';
 export const NUM_REQUESTED_PHOTOS = 12;
 
-export const getAllPhoto = async (photoTag, page) => {
-  console.log(page);
+export const getAllPhoto = async (photoTag, page) => {  
   const response = await axios.get(`${BASE_URL}`, {
     params: {
       key: `${API_KEY}`,
